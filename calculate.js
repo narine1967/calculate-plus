c5ffdc01493d863f2c530bf2609ed62d24c10f3e
narine1967 <narine.tsaturyan.67@mail.ru>
2019-08-13 17:35:48 +0400

    create plus calculate

---
+++ calculate.js
@@ -0,0 +1,11 @@
+
+
+function sumValues(){
+    var num1, num2, res;
+    num1=Number(document.formcalc.txtnum1.value);
+    num2=Number(document.formcalc.txtnum2.value);
+    res=num1+num2;
+    document.formcalc.txtres.value=res;
+}
+
+
